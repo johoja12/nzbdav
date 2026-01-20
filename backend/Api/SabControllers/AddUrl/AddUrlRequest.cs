aa4da91924e3e7c849cfd0a1f4dbe8b9d9da69ec
nzbdav-dev <nzbdavdev@protonmail.com>
2026-01-19 22:48:27 -0800

    Updated AddUrl api to fallback to url for determining nzb filename.

--- backend/Api/SabControllers/AddUrl/AddUrlRequest.cs
+++ backend/Api/SabControllers/AddUrl/AddUrlRequest.cs
@@ -46,14 +46,10 @@ public class AddUrlRequest() : AddFileRequest
             var contentType = response.Content.Headers.ContentType?.MediaType;
 
             // determine the filename
-            var fileName = AddNzbExtension(nzbName);
-            if (fileName == null)
-            {
-                var contentDisposition = response.Content.Headers.ContentDisposition;
-                fileName = contentDisposition?.FileName?.Trim('"');
-                if (string.IsNullOrEmpty(fileName))
-                    throw new Exception("Filename could not be determined from Content-Disposition header.");
-            }
+            var fileName = AddNzbExtension(nzbName)
+                           ?? GetFilenameFromResponseHeader(response)
+                           ?? GetFilenameFromUrl(url)
+                           ?? throw new Exception("Nzb filename could not be determined.");
 
             // read the file contents
             var fileContents = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
@@ -113,6 +109,29 @@ public class AddUrlRequest() : AddFileRequest
         var userAgent = EnvironmentUtil.GetEnvironmentVariable("NZB_GRAB_USER_AGENT") ?? DefaultUserAgent;
         httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
         return httpClient;
+    }
+
+    private static string? GetFilenameFromResponseHeader(HttpResponseMessage response)
+    {
+        var contentDisposition = response.Content.Headers.ContentDisposition;
+        var filename = contentDisposition?.FileName?.Trim('"');
+        return StringUtil.EmptyToNull(filename);
+    }
+
+    private static string? GetFilenameFromUrl(string url)
+    {
+        try
+        {
+            var filename = Path.GetFileName(new Uri(url).AbsolutePath);
+            if (string.IsNullOrWhiteSpace(filename)) return null;
+            filename = Uri.UnescapeDataString(filename);
+            filename = AddNzbExtension(filename);
+            return filename;
+        }
+        catch
+        {
+            return null;
+        }
     }
 
     private class NzbFileResponse
